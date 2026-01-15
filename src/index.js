9c34b5e52d8ad8caf048c77add299e805dc23817
Sahu_Keshav <thekc2083@gmail.com>
2026-01-15 09:16:28 +0530

    connection DB

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv"
+import connectDB from "./db/database.js";
+;
+// two things to remember 1: always remember to wrap things in try catch block and 2: that DB is in another country so we have to use async await
+
+
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB()
+
+/* the first approch to connect the database to main
+import express from "express";
+app = express()
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.error("Error connecting to the database", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is running on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Error connecting to the database", error);
+        throw error;
+    }
+})()
+
+*/
